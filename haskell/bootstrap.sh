69828b0a805b1e8b693d81424b9ee9a9119688dd
Shao Cheng <astrohavoc@gmail.com>
2018-01-08 06:52:19 +0800

    Add haskell nix packages to gc root

--- haskell/bootstrap.sh
+++ haskell/bootstrap.sh
@@ -4,9 +4,10 @@ source ~/.profile
 nix-env -i \
   bash \
   stack
-stack --nix --resolver nightly exec ghc -- --info
+stack --nix --nix-add-gc-roots --resolver nightly exec ghc -- --info
 
 nix-env --delete-generations old
+nix-collect-garbage
 sudo rm -rf \
   ~/.stack \
   /tmp/bootstrap.sh
